e3d03e3b5bb2c3a02a979564dd568f7c981f9b9f
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-07-23 08:07:33 +0700

    update LOGIN

---
+++ CRUD/myscript.js
@@ -0,0 +1,100 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  update,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCCUw6TLTvXNJth6TXcYXA_jnQT3w5Gg8g",
+  authDomain: "login-4c7ec.firebaseapp.com",
+  projectId: "login-4c7ec",
+  storageBucket: "login-4c7ec.appspot.com",
+  messagingSenderId: "937697811597",
+  appId: "1:937697811597:web:ff6cd56a3ebc1c9fd956c4",
+  measurementId: "G-ZK2Z8MNNC3",
+};
+
+var rollV, nameV, genderV, addressV;
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  console.log(rollV, nameV, addressV, genderV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .set({
+      rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+    });
+  alert("Data Inserted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .on("value", function (snap) {
+      document.getElementById("roll").value = snap.val().rollNo;
+      document.getElementById("name").value = snap.val().name;
+      document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("address").value = snap.val().address;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+    });
+  alert("Data Update");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+  alert("Data Deleted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
--- Login/login.js
+++ Login/login.js
@@ -1,6 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  update,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,145 +28,55 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const auth = getAuth();
 
-// Your web app's Firebase configuration
+// sign up
+let signUp = document.getElementById("signUp");
+signUp.addEventListener("click", function () {
+  let username = document.getElementById("usernameSignup").value;
+  let email = document.getElementById("emailSignUp").value;
+  let password = document.getElementById("passwordSignUp").value;
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// Initialize variables
-const auth = firebase.auth();
-const database = firebase.database();
-
-// Set up our register function
-function register() {
-  // Get all our input fields
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
-  full_name = document.getElementById("full_name").value;
-  favourite_song = document.getElementById("favourite_song").value;
-  milk_before_cereal = document.getElementById("milk_before_cereal").value;
-
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert("Email or Password is Outta Line!!");
-    return;
-    // Don't continue running the code
-  }
-  if (
-    validate_field(full_name) == false ||
-    validate_field(favourite_song) == false ||
-    validate_field(milk_before_cereal) == false
-  ) {
-    alert("One or More Extra Fields is Outta Line!!");
-    return;
-  }
-
-  // Move on with Auth
-  auth
-    .createUserWithEmailAndPassword(email, password)
-    .then(function () {
-      // Declare user variable
-      var user = auth.currentUser;
-
-      // Add this user to Firebase Database
-      var database_ref = database.ref();
-
-      // Create User data
-      var user_data = {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      set(ref(database, "users/" + user.uid), {
+        username: username,
         email: email,
-        full_name: full_name,
-        favourite_song: favourite_song,
-        milk_before_cereal: milk_before_cereal,
-        last_login: Date.now(),
-      };
-
-      // Push to Firebase Database
-      database_ref.child("users/" + user.uid).set(user_data);
-
-      // DOne
-      alert("User Created!!");
+      });
+      alert("user created");
     })
-    .catch(function (error) {
-      // Firebase will use this to alert of its errors
-      var error_code = error.code;
-      var error_message = error.message;
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
 
-      alert(error_message);
+      alert(errorMessage);
     });
-}
+});
 
-// Set up our login function
-function login() {
-  // Get all our input fields
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
+// login
+let login = document.getElementById("login");
+login.addEventListener("click", function () {
+  let email = document.getElementById("usernameLogin").value;
+  let password = document.getElementById("passwordLogin").value;
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert("Email or Password is Outta Line!!");
-    return;
-    // Don't continue running the code
-  }
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      let dt = new Date();
+      const user = userCredential.user;
+      update(ref(database, "users/" + user.uid), {
+        lastLogin: dt,
+      });
 
-  auth
-    .signInWithEmailAndPassword(email, password)
-    .then(function () {
-      // Declare user variable
-      var user = auth.currentUser;
-
-      // Add this user to Firebase Database
-      var database_ref = database.ref();
-
-      // Create User data
-      var user_data = {
-        last_login: Date.now(),
-      };
-
-      // Push to Firebase Database
-      database_ref.child("users/" + user.uid).update(user_data);
-
-      // DOne
-      alert("User Logged In!!");
+      alert("User login success");
+      // ...
     })
-    .catch(function (error) {
-      // Firebase will use this to alert of its errors
-      var error_code = error.code;
-      var error_message = error.message;
-
-      alert(error_message);
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
     });
-}
-
-// Validate Functions
-function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/;
-  if (expression.test(email) == true) {
-    // Email is good
-    return true;
-  } else {
-    // Email is not good
-    return false;
-  }
-}
-
-function validate_password(password) {
-  // Firebase only accepts lengths greater than 6
-  if (password < 6) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function validate_field(field) {
-  if (field == null) {
-    return false;
-  }
-
-  if (field.length <= 0) {
-    return false;
-  } else {
-    return true;
-  }
-}
+});
